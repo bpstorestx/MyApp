d3f6c30c5f18a18e3f012d8ca70fba5a5a592bd6
Billy <bpstorestx@gmail.com>
2025-05-21 14:55:14 -0500

    Add Redis URL logging to debug connection issues

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 require 'sidekiq'
 
+# Log the Redis URL on startup to ensure it's being set properly
+Rails.logger.info("SIDEKIQ INIT: REDIS_URL present: #{ENV['REDIS_URL'].present?}")
+if ENV['REDIS_URL'].present?
+  Rails.logger.info("SIDEKIQ INIT: REDIS_URL: #{ENV['REDIS_URL']}")
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'), timeout: 5 }
 end
